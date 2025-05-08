0fea445f224773821f780d5dc775a31e73d975d6
Arturo Suárez <manuel.suarez@cimat.mx>
2025-05-08 12:01:49 -0600

    Patches have two bands VV, VH so we cant use imread therefore we change to rasterio to open and read the products image patches

--- phd/semester7/scripts_ipicyt_dataset/build_segmentation_patches.py
+++ phd/semester7/scripts_ipicyt_dataset/build_segmentation_patches.py
@@ -1,5 +1,6 @@
 import os
 import itertools
+import rasterio
 import numpy as np
 import pandas as pd
 
@@ -11,24 +12,31 @@ from tqdm import tqdm
 # Directories configuration
 home_path = os.path.expanduser("~")
 data_path = os.path.join(home_path, "data")
-src_path = os.path.join(data_path, "cimat", "dataset-cimat")
-dst_path = os.path.join(data_path, "cimat", "dataset-cimat", "segmentation")
+src_path = os.path.join(data_path, "cimat", "dataset-ipicyt")
+dst_path = os.path.join(data_path, "cimat", "dataset-ipicyt", "segmentation")
 # Initial configuration
-image_path = "image_norm"
-label_path = "mask_bin"
+image_path = "Oil"
+label_path = "Mask_oil"
 patch_size = 224
 
 Image.MAX_IMAGE_PIXELS = None
 
 # Definimos el modelo U-Net con un backbone preentrenado (ResNet)
-slurm_array_task_id = os.getenv("SLURM_ARRAY_TASK_ID")
-slurm_ntasks = os.getenv("SLURM_NTASKS")
-slurm_procid = os.getenv("SLURM_PROCID")
-slurm_task_pid = os.getenv("SLURM_TASK_PID")
+slurm_array_task_id = os.getenv("SLURM_ARRAY_TASK_ID", 1)
+slurm_ntasks = os.getenv("SLURM_NTASKS", 64)
+slurm_procid = os.getenv("SLURM_PROCID", 0)
+slurm_task_pid = os.getenv("SLURM_TASK_PID", 0)
 print("SLURM_ARRAY_TASK_ID: ", slurm_array_task_id)
 print("SLURM_NTASKS: ", slurm_ntasks)
 print("SLURM_PROCID: ", slurm_procid)
 print("SLURM_TASK_PID: ", slurm_task_pid)
+
+
+def scale_image(image):
+    min_image = np.min(image)
+    max_image = np.max(image)
+    image_scaled = (image - min_image) / (max_image - min_image)
+    return image_scaled
 
 
 def patchify_image(
@@ -50,19 +58,20 @@ def patchify_image(
     # In this case we are opening both image and mask to patchify at the same time
     # considering that we are removing outside regions pixels (SAR image) and separating
     # oil from not oil spill patches
-    image = imread(os.path.join(src_path, img_dir, img_name + ".tif"), as_gray=True)
-    mask = imread(os.path.join(src_path, mask_dir, img_name + ".png"), as_gray=True)
+    product = rasterio.open(os.path.join(src_path, img_dir, img_name + ".tif"))
+    mask = rasterio.open(os.path.join(src_path, mask_dir, img_name + ".tif"))
+
+    image_vh = product.read(1)
+    image_vv = product.read(2)
+    mask = mask.read(1)
     # Scale image between 0 and 1
-    min_image = np.min(image)
-    max_image = np.max(image)
-    image_scaled = (image - min_image) / (max_image - min_image)
-    # Mark all pixels on the mask above 0 as oil
-    mask[mask > 0] = 1
+    image_vh_scaled = scale_image(image_vh)
+    image_vv_scaled = scale_image(image_vv)
 
     # Verifying that image and mask have the same shape
-    image_height, image_width = image.shape
+    image_height, image_width = image_vh.shape
     mask_height, mask_width = mask.shape
-    print("Image shape: ", image.shape)
+    print("Image shape: ", image_vh.shape)
     print("Mask shape: ", mask.shape)
     if (image_height != mask_height) or (image_width != mask_width):
         print("Error, image and mask must have the same dimensions")
@@ -104,16 +113,13 @@ def patchify_image(
         print("Patch size: ", patch_size)
 
         # Original image patch
-        image_patch = image[y : y + patch_size, x : x + patch_size]
-        print("Image patch shape: ", image_patch.shape)
+        image_vh_patch = image_vh[y : y + patch_size, x : x + patch_size]
+
+        print("Image patch shape: ", image_vh_patch.shape)
         # Scaled image patch
-        image_scaled_patch = image_scaled[y : y + patch_size, x : x + patch_size]
+        image_vh_scaled_patch = image_vh_scaled[y : y + patch_size, x : x + patch_size]
+        image_vv_scaled_patch = image_vv_scaled[y : y + patch_size, x : x + patch_size]
         total_patches = total_patches + 1
-        min_image_patch = np.min(image_patch)
-        max_image_patch = np.max(image_patch)
-        # We are checking if patch image values are 0, if so then continue next patch (we are in an invalid SAR image patch)
-        if min_image_patch == 0 and max_image_patch == 0:
-            continue
         dst_img_name = img_name + f"_{patch_index:04d}_train"
         mask_patch = mask[y : y + patch_size, x : x + patch_size]
         print("Mask patch shape: ", mask_patch.shape)
@@ -128,24 +134,41 @@ def patchify_image(
             continue
 
         imsave(
-            os.path.join(dst_path, "features", "origin", dst_img_name + ".tif"),
-            image_scaled_patch,
+            os.path.join(dst_path, "features", "origin_vh", dst_img_name + ".tif"),
+            image_vh_scaled_patch,
+            check_contrast=False,
+        )
+        imsave(
+            os.path.join(dst_path, "features", "origin_vv", dst_img_name + ".tif"),
+            image_vv_scaled_patch,
             check_contrast=False,
         )
         # Save in png for visualization
-        image_to_save = Image.fromarray((image_scaled_patch * 255).astype(np.int16))
-        image_to_save.save(os.path.join(dst_path, "images", dst_img_name + ".png"))
+        image_vh_to_save = Image.fromarray(
+            (image_vh_scaled_patch * 255).astype(np.int16)
+        )
+        image_vh_to_save.save(
+            os.path.join(dst_path, "images", dst_img_name + "_vh.png")
+        )
+        image_vv_to_save = Image.fromarray(
+            (image_vv_scaled_patch * 255).astype(np.int16)
+        )
+        image_vv_to_save.save(
+            os.path.join(dst_path, "images", dst_img_name + "_vv.png")
+        )
         imsave(
             os.path.join(dst_path, "labels", dst_img_name + ".png"),
             mask_patch,
             check_contrast=False,
         )
         # Save figure with image and mask patches
-        fig, ax = plt.subplots(1, 2, figsize=(12, 8))
-        ax[0].imshow(image_scaled_patch, cmap="gray")
-        ax[0].set_title("Image patch")
-        ax[1].imshow(mask_patch, cmap="gray")
-        ax[1].set_title("Mask patch")
+        fig, ax = plt.subplots(1, 3, figsize=(12, 8))
+        ax[0].imshow(image_vh_scaled_patch, cmap="gray")
+        ax[0].set_title("Image VH patch")
+        ax[1].imshow(image_vv_scaled_patch, cmap="gray")
+        ax[1].set_title("Image VV patch")
+        ax[2].imshow(mask_patch, cmap="gray")
+        ax[2].set_title("Mask patch")
         fig.tight_layout()
         fig.suptitle(dst_img_name)
         plt.savefig(os.path.join(dst_path, "figures", dst_img_name + ".png"))
@@ -154,18 +177,25 @@ def patchify_image(
 
 # Create output directories
 os.makedirs(dst_path, exist_ok=True)
-os.makedirs(os.path.join(dst_path, "features", "origin"), exist_ok=True)
+os.makedirs(os.path.join(dst_path, "features", "origin_vh"), exist_ok=True)
+os.makedirs(os.path.join(dst_path, "features", "origin_vv"), exist_ok=True)
 os.makedirs(os.path.join(dst_path, "images"), exist_ok=True)
 os.makedirs(os.path.join(dst_path, "labels"), exist_ok=True)
 os.makedirs(os.path.join(dst_path, "figures"), exist_ok=True)
 
-fname = os.listdir(os.path.join(src_path, "image_norm"))[int(slurm_array_task_id) - 1]
-patchify_image(
-    src_path,
-    "image_norm",
-    "mask_bin",
-    dst_path,
-    fname.split(".")[0],
-    patch_size,
+# There are 1200 images for IPICYT dataset so we need to divide them between 12 task arrays (100 images each one)
+indexes = range(
+    (int(slurm_array_task_id) - 1) * 100, int(slurm_array_task_id) * 100 - 1
 )
+
+for index in indexes:
+    fname = os.listdir(os.path.join(src_path, "Oil"))[index]
+    patchify_image(
+        src_path,
+        "Oil",
+        "Mask_oil",
+        dst_path,
+        fname.split(".")[0],
+        patch_size,
+    )
 print("Done!")
